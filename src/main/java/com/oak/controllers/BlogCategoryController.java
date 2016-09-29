1096545b2df679212e01aca7fa5a52e89c330a32
sharmamohitk <mohit12121@gmail.com>
2016-09-29 10:02:14 +0530

    blog category admin changes for image upload
    blog category admin changes for image upload

--- src/main/java/com/oak/controllers/BlogCategoryController.java
+++ src/main/java/com/oak/controllers/BlogCategoryController.java
@@ -20,13 +20,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.oak.entities.BlogCategory;
 import com.oak.service.BlogCategoryService;
+import com.oak.service.ImageService;
 import com.oak.vo.BlogCategoryVO;
 
 @RestController
@@ -35,10 +37,13 @@ public class BlogCategoryController {
 	@Autowired
 	BlogCategoryService blogCategoryService;
 
+	@Autowired
+	ImageService imageService;
+
 	@CrossOrigin
 	@RequestMapping(value = "/blog_categories", produces = "application/json", method = RequestMethod.GET)
-	public List<BlogCategoryVO> getAllBlogCategories() throws JsonParseException,
-			JsonMappingException, IOException {
+	public List<BlogCategoryVO> getAllBlogCategories()
+			throws JsonParseException, JsonMappingException, IOException {
 		List<BlogCategory> categories = blogCategoryService.getBlogCategories();
 		if (categories == null || categories.isEmpty()) {
 			return null;
@@ -65,17 +70,39 @@ public class BlogCategoryController {
 	}
 
 	@CrossOrigin
-	@RequestMapping(value = "/blog_categories", consumes = "application/json", method = RequestMethod.POST)
-	public ResponseEntity<Void> createBlogCategory(@RequestBody BlogCategoryVO categoryVO,
-			UriComponentsBuilder ucBuilder) throws JsonParseException,
-			JsonMappingException, IOException {
+	@RequestMapping(value = "/blog_categories", method = RequestMethod.POST)
+	public ResponseEntity<Void> createBlogCategory(
+			@RequestParam("name") String name,
+			@RequestParam("description") String description,
+			@RequestParam(name = "displayImage", required = false) MultipartFile displayImage)
+			throws JsonParseException, JsonMappingException, IOException {
 
-		categoryVO.setId(new Date().getTime());
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		System.out.println(name);
+		System.out.println(description);
+		System.out.println(displayImage);
+		System.out.println();
+		Authentication authentication = SecurityContextHolder.getContext()
+				.getAuthentication();
 		String email = authentication.getName();
-		
+		String id = null;
+		try {
+			byte[] imgbytes = displayImage.getBytes();
+			if (imgbytes != null) {
+				id = imageService.saveImage("blogimage",
+						displayImage.getOriginalFilename(),
+						displayImage.getSize(), displayImage.getBytes(), email);
+			}
+		} catch (NullPointerException npe) {
+			// do nothing
+		}
+		BlogCategoryVO categoryVO = new BlogCategoryVO();
+		categoryVO.setId(new Date().getTime());
 		categoryVO.setCreatedby(email);
-		categoryVO.setCreatedon(new Date().getTime());		
+		categoryVO.setCreatedon(new Date().getTime());
+		categoryVO.setName(name);
+		categoryVO.setDescription(description);
+		categoryVO.setDisplayimage("http://dev.insodel.com:6767/image/" + id);
+
 		blogCategoryService.createBlogCategory(new BlogCategory(categoryVO));
 		HttpHeaders headers = new HttpHeaders();
 		return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
@@ -83,12 +110,13 @@ public class BlogCategoryController {
 
 	@CrossOrigin
 	@RequestMapping(value = "/blog_categories/{id}", consumes = "application/json", method = RequestMethod.PUT)
-	public ResponseEntity<BlogCategoryVO> updateBlogCategory(@PathVariable("id") long id,
-			@RequestBody BlogCategoryVO categoryVO) throws JsonGenerationException,
-			JsonMappingException, IOException {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+	public ResponseEntity<BlogCategoryVO> updateBlogCategory(
+			@PathVariable("id") long id, @RequestBody BlogCategoryVO categoryVO)
+			throws JsonGenerationException, JsonMappingException, IOException {
+		Authentication authentication = SecurityContextHolder.getContext()
+				.getAuthentication();
 		String email = authentication.getName();
-		
+
 		categoryVO.setUpdatedby(email);
 		categoryVO.setUpdatedon(new Date().getTime());
 		blogCategoryService.updateBlogCategory(new BlogCategory(categoryVO));
@@ -98,7 +126,8 @@ public class BlogCategoryController {
 
 	@CrossOrigin
 	@RequestMapping(value = "/blog_categories/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<BlogCategoryVO> deleteBlogCategory(@PathVariable("id") long id) {
+	public ResponseEntity<BlogCategoryVO> deleteBlogCategory(
+			@PathVariable("id") long id) {
 		System.out.println("Fetching & Deleting Blog Category with id " + id);
 
 		BlogCategory category = blogCategoryService.getBlogCategoryById(id);
